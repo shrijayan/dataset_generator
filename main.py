0364c2c5984d8735d0f2c1ead175bb7073bf97ca
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-06 23:44:49 +0530

    Refactor main.py to remove unused code

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 from src import GenerateQA, TextExtractor
 from src import FileProcessor
-from src import QuestionValidator
 from src import HeaderFooterCleaner
 
 # Load environment variables from .env file
@@ -14,9 +13,6 @@ texts = extractor.extract_text_from_folder(input_folder)
 for text in texts:
     qa_pairs = GenerateQA()
     all_questions = qa_pairs.generate_questions(text)
-    
-    # validator = QuestionValidator()
-    # validator.validate_questions(all_questions)
     
     cleaner = HeaderFooterCleaner()
     cleaned_text = cleaner.remove_unwanted_lines(all_questions)
