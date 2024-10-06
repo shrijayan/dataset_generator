8abfe2a3616be6432a40a203477b1983810fb199
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-06 20:17:10 +0530

    Refactor cleanHeader.py to improve code readability and flexibility

--- src/cleanHeader.py
+++ src/cleanHeader.py
@@ -1,12 +1,12 @@
-class Clean_Headers_Footers:
-    def __init__(self, text):
-        self.text = text
-    def clean_up_questions(self):
-        first_line_remove_words = ['Here', 'I']
-        last_line_remove_words = ['Let', 'Note:', 'Please', 'If', '(Note:']
-        lines = self.text.split('\n')
-        if any(lines[0].startswith(word) for word in first_line_remove_words):
+class HeaderFooterCleaner:
+    def __init__(self, first_line_remove_words=None, last_line_remove_words=None):
+        self.first_line_remove_words = first_line_remove_words or ['Here', 'I']
+        self.last_line_remove_words = last_line_remove_words or ['Let', 'Note:', 'Please', 'If', '(Note:']
+
+    def remove_unwanted_lines(self, text):
+        lines = text.split('\n')
+        if any(lines[0].startswith(word) for word in self.first_line_remove_words):
             lines = lines[1:]
-        if any(lines[-1].startswith(word) for word in last_line_remove_words):
+        if any(lines[-1].startswith(word) for word in self.last_line_remove_words):
             lines = lines[:-1]
         return '\n'.join(lines)
