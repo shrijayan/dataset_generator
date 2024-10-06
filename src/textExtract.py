4b3a4329228280d823752c3908cd7f53a2630358
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-06 19:09:10 +0530

    Refactor TextExtractor to include folder_path parameter in the constructor and use it in extract_text_from_folder method

--- src/textExtract.py
+++ src/textExtract.py
@@ -1,12 +1,15 @@
 import os
 
 class TextExtractor:
-    def extract_text_from_folder(folder_path):
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        
+    def extract_text_from_folder(self):
         extracted_texts = []
         
         # List all files in the folder
-        for file_name in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, file_name)
+        for file_name in os.listdir(self.folder_path):
+            file_path = os.path.join(self.folder_path, file_name)
             
             # Check if it's a file
             if os.path.isfile(file_path):
