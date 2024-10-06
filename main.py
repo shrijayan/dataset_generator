71b608c7c76758064e2f981038df5acb720483d7
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-06 18:30:36 +0530

    Refactor main.py to remove unnecessary imports and unused classes

--- main.py
+++ main.py
@@ -1,19 +1,39 @@
 from openai import OpenAI
+from src import GenerateQA, TextExtractor, OpenAIClient
 
 if __name__ == '__main__':
-    # Define the input and output folders
+    
     input_folder = "cleaned_data"
     output_folder = "question"
 
-    # Initialize generator and validator
     openai_api_key = "YOUR_API_KEY"
+    # openai_api_base = "http://10.132.3.11:8000/v1"
     openai_api_base = "http://10.132.3.11:8000/v1"
     model_name = "meta-llama/Meta-Llama-3-8B-Instruct"
     client = OpenAI(api_key=openai_api_key, base_url=openai_api_base)
     
-    validator = QuestionValidator(client, model_name)
-    question_generator = OpenAIQuestionGenerator(client, model_name, validator, max_retries=10)
+    # Example usage for Ollama
+    ollama_api_url = 'http://localhost:11434/v1'
+    ollama_model_name = 'llama2'
+    ollama_api_key = 'ollama'  # Required but not used
+    ollama_system_message = "You are a helpful assistant."
+    ollama_prompt = "Who won the world series in 2020?"
 
-    # Process the files
-    file_processor = FileProcessor(question_generator)
-    file_processor.process_files(input_folder, output_folder)
+    # Initialize the client
+    client = OpenAIClient(api_url="http://10.132.3.11:11434/v1", api_key="your_api_key")
+
+    # Query the model
+    response = client.query_model(prompt="Hello, how are you?", model_name="llama3.1")
+    print(response)
+    
+    # Example usage
+    # texts = TextExtractor(input_folder)
+    # for text in texts:
+    #     qa_pairs = GenerateQA(client, model_name, text)
+    
+    # validator = QuestionValidator(client, model_name)
+    # question_generator = OpenAIQuestionGenerator(client, model_name, validator, max_retries=10)
+
+    # # Process the files
+    # file_processor = FileProcessor(question_generator)
+    # file_processor.process_files(input_folder, output_folder)
