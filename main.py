25aacaef21a6d486b828515802cd22ab80b9a50f
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-06 20:34:13 +0530

    Refactor main.py to remove unused code and update method signature

--- main.py
+++ main.py
@@ -6,18 +6,8 @@ from src import GenerateQA, TextExtractor, OpenAIClient
 load_dotenv()
     
 input_folder = "input_data"
+texts = TextExtractor.extract_text_from_folder(input_folder)
 
-# Initialize the client
-client = OpenAIClient(api_url=os.getenv("API_URL"), 
-                        api_key=os.getenv("API_KEY"), 
-                        model_name="llama3.1",
-                        )
-
-# Query the model
-response = client.query_model(prompt="Hello, how are you?")
-
-extractor = TextExtractor(input_folder)
-texts = extractor.extract_text_from_folder()
 for text in texts:
-    qa_pairs = GenerateQA(client, text)
+    qa_pairs = GenerateQA(text)
     qa_pairs.generate_questions()
