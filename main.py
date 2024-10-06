a110b40f86d5590f8ab54ee4b114e276de5b3d43
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-07 02:03:20 +0530

    Refactor main.py to use multiprocessing for parallel file processing

--- main.py
+++ main.py
@@ -1,31 +1,39 @@
-import os
+import json
+import multiprocessing
 from tqdm import tqdm
 from dotenv import load_dotenv
-from src import GenerateQA, TextExtractor
-from src import FileProcessor
-from src import HeaderFooterCleaner
+from src import GenerateQA, TextExtractor, FileProcessor, HeaderFooterCleaner
 
 # Load environment variables from .env file
 load_dotenv()
-    
-input_folder = "input_data"
-extractor = TextExtractor()
-texts, num_files = extractor.extract_text_from_folder(input_folder)
 
-progress_bar = tqdm(total=num_files, desc="Processing files")
-
-for text, input_file_name in texts:
+def process_file(text_and_filename):
+    text, input_file_name = text_and_filename
     qa_pairs = GenerateQA()
     all_questions = qa_pairs.generate_questions(text)
     
     cleaner = HeaderFooterCleaner()
     cleaned_text = cleaner.remove_unwanted_lines(all_questions)
     
-    file_procssing = FileProcessor()
-    file_procssing.save_questions(cleaned_text, input_file_name)
-    
-    # Update the progress bar
-    progress_bar.update(1)
+    file_processing = FileProcessor()
+    file_processing.save_questions(cleaned_text, input_file_name)
 
-# Close the progress bar
-progress_bar.close()
+if __name__ == "__main__":
+    with open('config.json', 'r') as config_file:
+        config = json.load(config_file)
+        
+    extractor = TextExtractor()
+    texts, num_files = extractor.extract_text_from_folder(config.get('input_folder'))
+
+    # Create a pool of worker processes
+    num_processes = multiprocessing.cpu_count()
+    pool = multiprocessing.Pool(processes=num_processes)
+
+    # Use tqdm to create a progress bar
+    with tqdm(total=num_files, desc="Processed files") as pbar:
+        for _ in pool.imap_unordered(process_file, texts):
+            pbar.update()
+
+    # Close the pool of worker processes
+    pool.close()
+    pool.join()
