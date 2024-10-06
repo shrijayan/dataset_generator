433af2ba372a270aae97e2e46b01477a05074e91
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-07 01:31:47 +0530

    Refactor main.py to improve code readability and add progress bar

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+from tqdm import tqdm
 from dotenv import load_dotenv
 from src import GenerateQA, TextExtractor
 from src import FileProcessor
@@ -8,7 +10,9 @@ load_dotenv()
     
 input_folder = "input_data"
 extractor = TextExtractor()
-texts = extractor.extract_text_from_folder(input_folder)
+texts, num_files = extractor.extract_text_from_folder(input_folder)
+
+progress_bar = tqdm(total=num_files, desc="Processing files")
 
 for text, input_file_name in texts:
     qa_pairs = GenerateQA()
@@ -19,3 +23,9 @@ for text, input_file_name in texts:
     
     file_procssing = FileProcessor()
     file_procssing.save_questions(cleaned_text, input_file_name)
+    
+    # Update the progress bar
+    progress_bar.update(1)
+
+# Close the progress bar
+progress_bar.close()
