3c6235282458bf8a04535dc63b17ac63edb52c3e
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-06 23:14:05 +0530

    Refactor main.py to import HeaderFooterCleaner class and remove unwanted lines from generated questions

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 from src import GenerateQA, TextExtractor
 from src import FileProcessor
 from src import QuestionValidator
+from src import HeaderFooterCleaner
 
 # Load environment variables from .env file
 load_dotenv()
@@ -14,8 +15,11 @@ for text in texts:
     qa_pairs = GenerateQA()
     all_questions = qa_pairs.generate_questions(text)
     
-    validator = QuestionValidator()
-    validator.validate_questions(all_questions)
+    # validator = QuestionValidator()
+    # validator.validate_questions(all_questions)
+    
+    cleaner = HeaderFooterCleaner()
+    cleaned_text = cleaner.remove_unwanted_lines(all_questions)
     
     file_procssing = FileProcessor()
-    file_procssing.save_questions(all_questions)
+    file_procssing.save_questions(cleaned_text)
