f1b38db41594617aa3c269de96d8c9d3090eda4a
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-06 22:08:17 +0530

    Refactor __init__.py to import QuestionValidator class

--- src/__init__.py
+++ src/__init__.py
@@ -2,3 +2,4 @@ from .generateQA import GenerateQA
 from .textExtract import TextExtractor
 from .openai import OpenAIClient
 from .fileProcessing import FileProcessor
+from .questionValidator import QuestionValidator
