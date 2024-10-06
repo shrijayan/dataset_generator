5d0f6842b9cc77ec8afc00d1924acac544c76f85
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-07 01:06:03 +0530

    Refactor generateQA.py to use system prompt from separate file and improve question generation logic

--- src/generateQA.py
+++ src/generateQA.py
@@ -1,32 +1,39 @@
 from .cleanHeader import HeaderFooterCleaner
 from .openai import client
+from .tokenCounter import TokenCounter
+import textwrap 
 
 class GenerateQA:
+    def __init__(self):
+        self.tokenCounter = TokenCounter()
+        
+        with open('prompts/generateQA-sys_prompt.txt', 'r') as file:
+            self.sys_prompt = file.read()
+            
     def generate_questions(self, text):
         try:
             model_max_tokens = 32000
-            prompt_tokens = 3000
-            max_chunk_size = model_max_tokens - prompt_tokens
-            if len(text) > max_chunk_size:
-                chunks = [text[i:i+max_chunk_size] for i in range(0, len(text), max_chunk_size)]
-            else:
-                chunks = [text]
+            sys_prompt_tokens = self.tokenCounter.get_token_size(self.sys_prompt)
+            chunk_size_limit = model_max_tokens - sys_prompt_tokens
             
-            all_questions = []
-            for chunk in chunks:
-                chuck_qa = self.generate_chunk_questions(chunk)
-                all_questions.append(chuck_qa)
-                
-            return '\n'.join(all_questions)
+            chunks = textwrap.wrap(text, width=chunk_size_limit, break_long_words=False, replace_whitespace=False)
+            
+            all_questions = [question for chunk in chunks for question in self.generate_chunk_questions(chunk)]
+            
+            # Return all questions combined as a string
+            return ''.join(all_questions)
+        
+        except Exception as e:
+            # Handle and log the exception (e.g., logging module)
+            print(f"Error generating questions: {str(e)}")
+            return ""
+
         
         except Exception as e:
             print(f"Error generating questions: {str(e)}")
             return None
     
     def generate_chunk_questions(self, content):
-        with open('prompts/generateQA-sys_prompt.txt', 'r') as file:
-            sys_prompt = file.read()
-            
-        response = client.query_model(content, sys_prompt)
+        response = client.query_model(content, self.sys_prompt)
         
         return response
