562b0e0627a27db55740cc55e2721c8b8883acba
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-08 17:04:07 +0530

    Refactor cleanHeader.py to remove unused code

--- src/fileHandle/cleanHeader.py
+++ src/fileHandle/cleanHeader.py
@@ -1,8 +1,4 @@
 class HeaderFooterCleaner:
-    def __init__(self, first_line_remove_words=None, last_line_remove_words=None):
-        self.first_line_remove_words = first_line_remove_words or ['Here', 'I', 'Based','```']
-        self.last_line_remove_words = last_line_remove_words or ['Let', 'Note:', 'Please', 'If', '(Note:','```']
-
     def remove_unwanted_lines(self, text):
         lines = text.split('\n')
         
