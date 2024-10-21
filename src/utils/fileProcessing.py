864f2f2a218b26753d2dbb8a9689abecd2e69d66
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-21 19:43:32 +0530

    Add FileProcessor class for saving questions to a JSONL file

---
+++ src/utils/fileProcessing.py
@@ -0,0 +1,11 @@
+import os
+
+class FileProcessor():
+    def save_file(self, questions, input_file_name, output_folder="generated_questions"):
+        if not os.path.exists(output_folder):
+            os.makedirs(output_folder)
+
+        output_file_path = os.path.join(output_folder, f"{input_file_name}.jsonl")
+
+        with open(output_file_path, 'w', encoding='utf-8') as file:
+            file.write(questions)
