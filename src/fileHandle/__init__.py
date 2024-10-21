60aa09c01ea800c6500cf2b04a68b124173c9c0f
shrijayan <81805145+shrijayan@users.noreply.github.com>
2024-10-21 19:42:15 +0530

    Refactor fileHandle module to remove unused code

--- src/fileHandle/__init__.py
+++
@@ -1,5 +0,0 @@
-from .cleanHeader import HeaderFooterCleaner
-from .fileProcessing import FileProcessor
-from .textExtract import TextExtractor
-from .folderTextReader import FolderTextReader
-from .duplicateCheck import duplicateCheck
